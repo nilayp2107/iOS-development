945bbd2332df40fe8afde25708c6b760927755d8
nilayp2107 <0aayyy2107@gmail.com>
2023-08-05 15:39:01 +0530

    Video Player commit

---
+++ video_player/video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Nilay Patel on 05/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
